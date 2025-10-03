81365bdf3e5b00f28da39c2261323769c1912965
Julian Kaung <minkgkyaw9899@gmail.com>
2025-10-03 22:43:36 +0630

    feat: learned dice roller app with images and button

--- app/src/main/java/com/mkk/diceroller/MainActivity.kt
+++ app/src/main/java/com/mkk/diceroller/MainActivity.kt
@@ -4,44 +4,93 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mkk.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            DiceRollerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      DiceRollerTheme {
+        Scaffold { paddingValues ->
+          DiceRollerApp(
+            modifier = Modifier
+              .fillMaxSize()
+              .wrapContentSize(Alignment.Center)
+              .padding(paddingValues)
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DiceRollerApp(modifier: Modifier) {
+  var result by remember {
+    mutableIntStateOf(1)
+  }
+
+  val imageResource = when (result) {
+    1 -> R.drawable.dice_1
+    2 -> R.drawable.dice_2
+    3 -> R.drawable.dice_3
+    4 -> R.drawable.dice_4
+    5 -> R.drawable.dice_5
+    else -> R.drawable.dice_6
+  }
+
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+  ) {
+    Image(
+      painter = painterResource(imageResource),
+      contentDescription = result.toString()
     )
+    Spacer(modifier = Modifier.height(24.dp))
+    Button(
+      onClick = {
+        result = (1..6).random()
+      }
+    ) {
+      Text(
+        stringResource(R.string.roll),
+        modifier = Modifier.padding(horizontal = 32.dp)
+      )
+    }
+  }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    DiceRollerTheme {
-        Greeting("Android")
-    }
+fun DiceRollerAppPreview() {
+  DiceRollerApp(
+    modifier = Modifier
+      .fillMaxSize()
+      .wrapContentSize(Alignment.Center)
+  )
 }
